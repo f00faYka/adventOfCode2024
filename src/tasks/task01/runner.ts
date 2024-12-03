bac19ab188396267fed93f80767e280ffec2f8fc
NIKOLAY LOZOVSKOY <nlozovskoy@evolution.com>
2024-12-03 11:59:59 +0500

    Task02

--- src/tasks/task01/runner.ts
+++ src/tasks/task01/runner.ts
@@ -3,5 +3,5 @@ import { part1, part2 } from './index';
 
 const input = parseInput(__dirname, 'input.txt');
 
-console.log('Part 1:', part1(input));
-console.log('Part 2:', part2(input));
+// console.log('Part 1:', part1(input));
+// console.log('Part 2:', part2(input));
---
+++ src/tasks/task02/index.ts
@@ -0,0 +1,65 @@
+import { all, any, both, compose, curry, either, filter, head, length, map, pipe, slice, split, tail, tap, trim, zip } from 'ramda';
+import { parseInput } from '../../utils/input';
+
+type Pair = [number, number];
+
+const parseLine = pipe(
+    trim,
+    split(/\s+/),
+    map(Number)
+) as () => number[];
+
+const parseInputIntoArrays = pipe(
+    parseInput,
+    map(parseLine),
+);
+
+const isDampenedArraysSafe = (numbers: number[]): boolean => {
+    const dampenedArrays: number[][] = [];
+    for (let i = 0; i < numbers.length; i++){
+        dampenedArrays.push([...numbers.slice(0, i), ...numbers.slice(i + 1)])
+    }
+
+    return any(isSafe)(dampenedArrays);
+}
+
+const isWrongDirection = curry((isAsc: boolean, [a, b]: Pair) => isAsc ? a > b : a < b);
+const hasLargeGap = ([a, b]: Pair) => Math.abs(b - a) >= 4;
+const createValidator = (isAsc: boolean) => either(
+    isWrongDirection(isAsc),
+    hasLargeGap,
+);
+const hasCorrectLength = (numbers: number[]) => length(numbers) >= 2;
+
+const isSafe = (numbers: number[]): boolean => {
+    if (!hasCorrectLength(numbers)) return false;
+    const isInvalidPair = createValidator(numbers[0] < numbers[1]);
+
+    const pairs = zip(numbers.slice(0, -1), numbers.slice(1));
+    return !pairs.some(isInvalidPair);
+}
+
+const isSafeWithDampener = (numbers: number[]): boolean => {
+    if (!hasCorrectLength(numbers)) return false;
+    const isInvalidPair = createValidator(numbers[0] < numbers[1]);
+
+    const pairs = zip(numbers.slice(0, -1), numbers.slice(1));
+    if (pairs.some(isInvalidPair)) {
+        return isDampenedArraysSafe(numbers);
+    }
+
+    return true;
+}
+
+const getSafeReportCount = pipe(
+    filter(isSafe),
+    length as (x: any[]) => number,
+)
+
+const getSafeReportCountWithDampener = pipe(
+    filter(isSafeWithDampener),
+    length as (x: any[]) => number,
+)
+
+console.log(compose(getSafeReportCount, parseInputIntoArrays)("./src/tasks/task02", "./input.txt"));
+console.log(compose(getSafeReportCountWithDampener, parseInputIntoArrays)("./src/tasks/task02", "./input.txt"));
